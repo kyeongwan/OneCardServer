25ac80716299037f4b58c859d52d1cee36b2b047
kyeongwan <lk@lemonlab.co.kr>
2016-05-30 15:51:16 +0900

    Update Test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,13 +1,15 @@
+import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Vertx;
 
 /**
  * Created by lk on 16. 4. 28..
  */
-public class Test {
+public class Test extends AbstractVerticle {
 
     public static void main(String argp[]){
         Vertx v = Vertx.vertx();
         v.deployVerticle(new HttpServerVertical());
         v.deployVerticle(new DBVerticle());
+        v.deployVerticle(new ChatVerticle());
     }
 }
